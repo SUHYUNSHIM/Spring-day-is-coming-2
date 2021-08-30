ff0fa73f88a79388ffb179eef905825198613aa0
suhyunshim <lisa9963suhyun@gmail.com>
2021-08-30 21:42:38 +0900

    직전, 직후 보안 처리

---
+++ HelloSpringMaven9/src/main/java/helloProcess/Security.java
@@ -0,0 +1,11 @@
+package helloProcess;
+
+public class Security {
+	public void goSecurity() {
+		System.out.println("공통: 여기는 직전 보안 처리 중");
+	}
+	public void goSecurity2() {
+		System.out.println("공통: 여기는 나중 보안 처리 중");
+		System.out.println();
+	}
+}
