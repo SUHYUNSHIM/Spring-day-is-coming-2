857f4c65b571ac4c4ed89e1568d87a4c7a9bdc24
suhyunshim <lisa9963suhyun@gmail.com>
2021-09-01 00:30:46 +0900

    mvc practice

---
+++ HelloMVC2/src/main/java/com/suhyun/mvc2/HomeController.java
@@ -0,0 +1,47 @@
+package com.suhyun.mvc2;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	/*@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}*/
+	@RequestMapping(value="/neotech.do", method= RequestMethod.POST)
+	public String neoTech(Model model, HttpServletRequest req) { //input에서 받을 떄 servlet으로 받는다.
+		String irum = req.getParameter("irum");
+		model.addAttribute("n_irum",irum); //view로 넘길 떄 model이 가동된다.
+		return "neo";
+	}
+	
+}
---
+++ HelloMVC2/src/main/webapp/WEB-INF/views/neo.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>Insert title here</title>
+</head>
+<body>
+<h1> 당신의 이름은 ${n_irum}</h1>
+</body>
+</html>
