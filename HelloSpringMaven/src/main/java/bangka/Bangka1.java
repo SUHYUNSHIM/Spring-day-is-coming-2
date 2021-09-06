571d007d551dbcf0b0b8286487afa5f9ca19dab4
suhyunshim <lisa9963suhyun@gmail.com>
2021-09-06 13:02:58 +0900

    comment

--- HelloSpringMaven/src/main/java/bangka/Bangka1.java
+++ HelloSpringMaven/src/main/java/bangka/Bangka1.java
@@ -1,10 +1,17 @@
 package bangka;
 
-public class Bangka1 {
+public class Bangka1 { //src 아래에 위치.
 
 	public void bangkabangka1(String name1) {
 		System.out.println("방가방가"+name1);
 
+		/*
+		 * Bangka` bangkaBean1 = new Bangka1();
+		 * <bean id="bangkaBean1" class="bangka.Bangka1"/>
+		 * 이건 application1.xml 파일에서 객체 bangkaBEan1이 생성된 것을 보여줌
+		 * 그러므로 스프링 객체는 클래스 내에서 생성하지  않고, xml에서 생성된다.
+		 * */
+		
 	}
 
 }
--- HelloSpringMaven/src/main/java/hello/Hello.java
+++ HelloSpringMaven/src/main/java/hello/Hello.java
@@ -7,10 +7,14 @@ import bangka.Bangka1;
 
 public class Hello {
 	public static void main(String[] args) {
-		ApplicationContext ac1 = new ClassPathXmlApplicationContext("applicationContext.xml");
-	
-		Bangka1 b1 = (Bangka1) ac1.getBean("bangkaBean1");
+		//bean은 spring이 관리하는 인스턴스
+		//ApplicationContext = bean container = bean을 관리
+		ApplicationContext ac1 = new ClassPathXmlApplicationContext("applicationContext.xml");	
+		
+		Bangka1 b1 = (Bangka1) ac1.getBean("bangkaBean1"); //위의 경로에서 만든 xml에서 bean을 찾아낸다. 여기서 new로 객체를 생성하지 않았음.
+		//Bangka1 bangkaBean1 = new Bangka1();		   //bean을 객체로 타입 변환. bean 객체 생성 및 초기화 제거 등을 관리함.
 		b1.bangkabangka1("홍길동");
+		
 	}
 
 }
